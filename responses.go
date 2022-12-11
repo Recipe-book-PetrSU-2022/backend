d6d878eda7652881c47ab5931c79be30152cba6b
Kvist Tatyana <rakabidasta@gmail.com>
2022-12-12 00:58:25 +0300

    added responses

--- responses.go
+++ responses.go
@@ -8,3 +8,18 @@ type TokenResponse struct {
 	Message string `json:"message"`
 	Token   string `json:"token"`
 }
+
+type ProfileResponse struct {
+	Message      string `json:"message"`
+	Id           uint   `json:"id"`
+	Username     string `json:"username"`
+	Email        string `json:"email"`
+	ProfilePhoto string `json:"photo"`
+}
+
+type UserResponse struct {
+	Message      string `json:"message"`
+	Id           uint   `json:"id"`
+	Username     string `json:"username"`
+	ProfilePhoto string `json:"photo"`
+}
