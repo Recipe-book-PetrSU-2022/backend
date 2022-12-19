ba2b22bf1a0c815937eddc44ca0c33f93365cb0b
Kvist Tatyana <rakabidasta@gmail.com>
2022-12-19 23:08:10 +0300

    update get recipe apis

--- models/user.go
+++ models/user.go
@@ -9,7 +9,7 @@ type User struct {
 	StrUserPassword string    `gorm:"not null" json:"-"`
 	StrUserEmail    string    `gorm:"index;unique;not null" json:"-"`
 	IntUserRights   int       `gorm:"not null;default:0" json:"-"`
-	StrUserImage    string    `gorm:"not null;default:0"`
+	StrUserImage    string    `gorm:"not null"`
 	UserRecipes     []Recipe  `gorm:"foreignKey:IntUserId" json:"-"`
 	UserComments    []Comment `gorm:"foreignKey:IntUserId" json:"-"`
 	UserFavorite    []Recipe  `gorm:"many2many:user_favorite_recipes" json:"-"`
