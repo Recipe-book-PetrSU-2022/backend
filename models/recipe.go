4f8c2d52ee7ea863d472fc65107577fdfd52776a
Kvist Tatyana <rakabidasta@gmail.com>
2022-12-19 20:43:34 +0300

    update models

--- models/recipe.go
+++ models/recipe.go
@@ -5,15 +5,16 @@ import "gorm.io/gorm"
 type Recipe struct {
 	gorm.Model
 
-	StrRecipeName     string             `gorm:"index;not null"`
-	IntServings       int                `gorm:"not null;default:0"`
-	IntTime           int                `gorm:"not null;default:0"`
-	StrRecipeCountry  string             `gorm:"not null"`
-	StrRecipeType     string             `gorm:"not null"`
-	StrRecipeImage    string             `gorm:"not null"`
-	IntUserId         uint               `gorm:"not null"`
-	User              User               `gorm:"foreignKey:IntUserId"`
-	RecipeStages      []Stage            `gorm:"foreignKey:IntRecipeId"`
-	RecipeComments    []Comment          `gorm:"foreignKey:IntRecipeId"`
-	RecipeIngredients []RecipeIngredient `gorm:"foreignKey:IntRecipeId"`
+	StrRecipeName        string             `gorm:"index;not null"`
+	IntServings          int                `gorm:"not null;default:0"`
+	IntTime              int                `gorm:"not null;default:0"`
+	StrRecipeCountry     string             `gorm:"not null"`
+	StrRecipeType        string             `gorm:"not null"`
+	StrRecipeImage       string             `gorm:"not null"`
+	BoolRecipeVisibility bool               `gorm:"not null"`
+	IntUserId            uint               `gorm:"not null"`
+	User                 *User              `gorm:"foreignKey:IntUserId" json:"user,omitempty"`
+	RecipeStages         []Stage            `gorm:"foreignKey:IntRecipeId"`
+	RecipeComments       []Comment          `gorm:"foreignKey:IntRecipeId"`
+	RecipeIngredients    []RecipeIngredient `gorm:"foreignKey:IntRecipeId"`
 }
--- models/recipe_ingredients.go
+++ models/recipe_ingredients.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type RecipeIngredient struct {
 	gorm.Model
 
-	IntCalories     int        `gorm:"not null;default:0"`
+	IntGrams        int        `gorm:"not null;default:0"`
 	IntRecipeId     uint       `gorm:"not null"`
 	Recipe          Recipe     `gorm:"foreignKey:IntRecipeId"`
 	IntIngredientId uint       `gorm:"not null"`
